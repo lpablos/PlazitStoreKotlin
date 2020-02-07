864bb613c973fe94555431a0a23e1033c89d19f3
Luis Jorge Pablo S <gevolutionxalapa@gmail.com>
2020-02-07 00:35:30 -0600

    Añadiendo listado con detalle

--- app/src/main/java/com/example/plazitstore/AdapterLanding.kt
+++ app/src/main/java/com/example/plazitstore/AdapterLanding.kt
@@ -1,8 +1,15 @@
 package com.example.plazitstore
 
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+
+
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_landing.view.*
 
@@ -28,6 +35,21 @@ class AdapterLanding ( val datos: List<ItemLanding> ) : RecyclerView.Adapter<Ada
                 itemView.textTitleItem.text = titulo
                 itemView.textDescItem.text = desc
                 itemView.textPriceItem.text = "$ ${String.format("%.2f",price)}"
+
+                itemView.setOnClickListener {
+                    val  intent = Intent(itemView.context, DetailActivity::class.java)
+                    intent.putExtra("title", titulo)
+                    intent.putExtra("description", desc)
+                    intent.putExtra("price", price)
+
+                    val p1:Pair<View,String> = Pair.create(itemView.imgItemHeader,"transitionHeader")
+                    val p2:Pair<View,String> = Pair.create(itemView.textTitleItem,"transitionTitle" )
+                    val p3:Pair<View,String> = Pair.create(itemView.textDescItem, "transitionDesc")
+                    val p4:Pair<View,String> = Pair.create(itemView.textPriceItem,"transitionPrice")
+
+                    val options: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity, p1, p2, p3, p4)
+                    itemView.context.startActivity(intent,options.toBundle())
+                }
             }
 
         }
--- app/src/main/java/com/example/plazitstore/DetailActivity.kt
+++ app/src/main/java/com/example/plazitstore/DetailActivity.kt
@@ -1,17 +1,24 @@
 package com.example.plazitstore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_descr.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        intent?.let {
+        /*intent?.let {
             val textFromMain: String? = it.extras?.getString("text")
             textDetail.text = textFromMain.toString()
+        }*/
+        intent?.extras?.let {
+            txtDetailTitulo.text = it.getString("title")
+            txtDetailDesc.text = it.getString("description")
+            txtDetailPrice.text = "$ ${String.format("%.2f",it.getDouble("price"))}"
         }
     }
 }
